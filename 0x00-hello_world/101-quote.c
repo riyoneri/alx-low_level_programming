24fe26e64397ccc9103b79ca6328abf964595f73
riyoneri <lionkaneza@gmail.com>
2023-03-18 00:37:51 -0700

    Add 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+* main - Entry point
+*
+* Return: Always 1 (Success)
+*/
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, str, sizeof(str) - 1);
+	return (1);
+}
