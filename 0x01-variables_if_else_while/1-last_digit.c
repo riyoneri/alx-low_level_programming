0ac0a0ee50ec652cc4406cbfb0e33f4ea9d4d57a
riyoneri <lionkaneza@gmail.com>
2023-03-18 01:14:10 -0700

    Remove absolute value to output

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,11 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	lastDigit = n % 10 < 0 ? (n % 10) * -1 : n % 10;
+	lastDigit = n % 10;
 
 	if (lastDigit == 0)
 		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-	else if (lastDigit > 0 && lastDigit < 6)
+	else if (lastDigit < 6)
 	{
 		printf("Last digit of %d is %d ", n, lastDigit);
 		printf("is less than 6 and not 0\n");
