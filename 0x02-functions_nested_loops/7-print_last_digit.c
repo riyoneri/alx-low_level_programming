76719100803f738f580181a5c72a058a5a1051de
riyoneri <lionkaneza@gmail.com>
2023-03-22 12:41:43 -0700

    Add 7-main.c, 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+*print_last_digit - print last digit of input value
+*
+*@num: number to get last value for
+*
+*Return: last number of value
+*/
+
+int print_last_digit(int num)
+{
+int number_mod;
+
+if (num < 0)
+{
+num *= -1;
+}
+
+number_mod = num % 10;
+
+_putchar(number_mod + '0');
+return (num % 10);
+}
