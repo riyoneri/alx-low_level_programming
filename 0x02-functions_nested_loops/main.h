65d27b37e8e367d92e85e8d706d97c1fbeec3af7
riyoneri <lionkaneza@gmail.com>
2023-03-22 00:02:02 -0700

    Add file guard in main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,29 @@
-#include <stdio.h>
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <unistd.h>
+
+/**
+*_putchar - print a single character
+*
+*/
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+
+/**
+*print_alphabet - print all lowercase alphabet 
+*
+*/
 void print_alphabet(void)
 {
-    int i;
-    for (i = 97; i <= 122; i++)
-    {
-        putchar(i);
-    }
-    putchar('\n');
+int i;
+for (i = 97; i <= 122; i++)
+{
+putchar(i);
 }
+putchar('\n');
+}
+#endif
