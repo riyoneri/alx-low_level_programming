bd34634a6994db5508379011b72ec4a07bc01752
riyoneri <lionkaneza@gmail.com>
2023-03-22 08:42:21 -0700

    Add _islower() description for betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,10 @@
 
 /**
 *_islower - check wheter character is lowercase or upper case
+*
+*@param c: character to be checked if is lowercase (character)
+*
+*@return value of 1 and 0 according to case of character (int) 
 */
 
 int _islower(char c)
