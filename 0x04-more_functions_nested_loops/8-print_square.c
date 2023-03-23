cf3977c8ab6efb00cc0154e87a13d10502a15f01
riyoneri <lionkaneza@gmail.com>
2023-03-23 14:50:02 -0700

    Add 8-main.c, 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* print_square - print # line
+* @n: number of # to print
+*/
+
+void print_square(int n)
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
