a8a330a96d8c0c0e26ecc0fdcce0098573c9d444
riyoneri <lionkaneza@gmail.com>
2023-03-23 03:54:11 -0700

    Add 2-main.c, 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * mul - print product of numbers
+ * @a: First number to calculate product for
+ * @b: Second number to calculate product for
+ * Return: product
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
