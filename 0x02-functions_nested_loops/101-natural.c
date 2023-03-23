a591cf637b03194a5de4afb87a9b2ef880d574a0
riyoneri <lionkaneza@gmail.com>
2023-03-22 19:47:05 -0700

    Add 101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+*main - print sum of natural numbers less than 1024 and divisible by 3 or 5
+*
+*Return: 0 success
+*/
+
+int main(void)
+{
+int i, sum = 0;
+
+for (i = 1; i < 1024; i++)
+{
+if ((i % 3 == 0) ^ (i % 5 == 0))
+{
+sum += i;
+}
+}
+
+printf("%d", sum);
+return (0);
+}
