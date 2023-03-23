d5bd60474e0ddbadc1c204e422590b94f056ca5f
riyoneri <lionkaneza@gmail.com>
2023-03-22 19:05:05 -0700

    Add 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,44 @@
-#include <stdio.h>
-void fibbo(){
+#include "main.h"
+
+/**
+*fibbo - printing first 98 fibonacci series
+*
+*Return: no return data
+*/
+
+void fibbo(void)
+{
 int num1 = 1, num2 = 2, num3, i;
-printf("%d, ", num1);
-printf("%d, ", num2);
-for(i = 0; i<98; i++){
+_putchar(num1 + '0');
+_putchar(',');
+_putchar(' ');
+_putchar(num2 + '0');
+_putchar(',');
+_putchar(' ');
+
+for (i = 0; i < 98; i++)
+{
 num3 = num1 + num2;
-printf("%d, ", num3);
-        
+
+void print_long(long num)
+{
+if (num < 0)
+{
+_putchar('-');
+num = -num;
+}
+
+if (num > 9)
+{
+print_long(num / 10);
+}
+_putchar(num % 10 + '0');
+
+}
+print_long(num3);
+_putchar(',');
+_putchar(' ');
+
 num1 = num2;
 num2 = num3;
 }
