a62aad1fa961cf5a1411084f3c8f368ae1f6c191
riyoneri <lionkaneza@gmail.com>
2023-03-23 14:32:03 -0700

    Add endline on 0 and less input

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,14 +8,22 @@
 void print_diagonal(int n)
 {
 int i, j;
-for (i = 1; i <= n; i++)
+
+if (n <= 0)
 {
-for (j = 1; j <= i; j++)
+_putchar('\n');
+}
+else
+{
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < i; j++)
 {
 _putchar(' ');
 }
 _putchar('\\');
 _putchar('\n');
+}
 }
 
 }
