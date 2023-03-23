6cdb21363fc875a61f290e37bd7a9a9da17aa41c
riyoneri <lionkaneza@gmail.com>
2023-03-23 14:53:51 -0700

    Tweak condition in for loop

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,6 +8,13 @@
 void print_square(int n)
 {
 int i, j;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
@@ -16,4 +23,6 @@ _putchar('#');
 }
 _putchar('\n');
 }
+}
+
 }
