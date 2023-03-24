6c07617581cafde66b0c97b571874aa78ed27eb2
riyoneri <lionkaneza@gmail.com>
2023-03-23 17:05:50 -0700

    Remove white spaces on last output 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,19 +13,19 @@ for (i = 1; i <= 100; i++)
 
 if ((i % 3 == 0) && (i % 5 == 0))
 {
-printf("FizzBuzz ");
+i == 100 ? printf("FizzBuzz") : printf("FizzBuzz ");
 continue;
 }
 
 if (i % 3 == 0)
 {
-printf("Fizz ");
+i == 100 ? printf("Fizz") : printf("Fizz ");
 continue;
 }
 
 if (i % 5 == 0)
 {
-printf("Buzz ");
+i == 100 ? printf("Buzz") : printf("Buzz ");
 continue;
 }
 
