b8e5ee14ed4c22c803b167146fa07e8432fa454b
riyoneri <lionkaneza@gmail.com>
2023-03-27 16:41:34 -0700

    Add 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swapping two integers using pointers
+ * @a: first integer
+ * @b: second integer
+*/
+
+void swap_int(int *a, int *b)
+{
+
+int temp;
+temp = *a;
+*a = *b;
+*b = temp;
+
+}
