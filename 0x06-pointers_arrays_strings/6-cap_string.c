50c05b4a41c51edf829042ec6fcb4ea65c6df124
riyoneri <lionkaneza@gmail.com>
2023-03-29 23:29:22 -0700

    Add 6-cap_string.c - 0x06

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+* cap_string - array to capitalize
+* @arr: array
+* Return: capitalized array
+*/
+
+char *cap_string(char *arr)
+{
+int i = 0;
+
+while (arr[i] != '\0')
+{
+
+if (
+(
+arr[i] == 32 ||
+arr[i] == 9 ||
+arr[i] == 10 ||
+arr[i] == 44 ||
+arr[i] == 59 ||
+arr[i] == 46 ||
+arr[i] == 33 ||
+arr[i] == 34 ||
+arr[i] == 63 ||
+arr[i] == 41 ||
+arr[i] == 42 ||
+arr[i] == 123 ||
+arr[i] == 125
+)
+&&
+(arr[i + 1] >= 97 && arr[i + 1] <= 122)
+)
+
+{
+
+arr[i + 1] = arr[i + 1] - 32;
+
+}
+
+i++;
+}
+
+return (arr);
+}
