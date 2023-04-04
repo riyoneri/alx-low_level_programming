336400292774368b8c5b79516aa407168969c627
riyoneri <lionkaneza@gmail.com>
2023-04-04 07:46:33 -0700

    Add 100-set_string.c - 0x07

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - replace pointer string
+ * @s: pointer to pointer of string
+ * @to: pointer to result
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,3 +7,5 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 
 char *_strpbrk(char *s, char *accept);
+
+void set_string(char **s, char *to);
